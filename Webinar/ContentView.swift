63f7c16a4cfe8ea701e1a18fc7462f1ff1685d57
Klaus M. Rodewig <kmr@appnoe.com>
2023-02-27 20:29:12 +0100

    add Text extension
    Signed-off-by: Klaus M. Rodewig <kmr@appnoe.com>

--- Webinar/ContentView.swift
+++ Webinar/ContentView.swift
@@ -9,32 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form{
-            Section{
-                Text("1")
-                Text("2")
-                Text("3")
-                Text("4")
-                Text("5")
-                Text("6")
-                Text("7")
-                Text("8")
-                Text("9")
-                Text("10")
-            } header: {
-                Text("First header")
-                    .fontWeight(.bold)
-            }
+        NavigationView {
+            Form{
+                Section{
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                    Text("4")
+                    Text("5")
+                    Text("6")
+                    Text("7")
+                    Text("8")
+                    Text("9")
+                    Text("10")
+                } header: {
+                    Text("First header")
+                        .sectionHeaderStyle()
+                }
 
-            Section{
-                Text("11")
-                Text("12")
-            } header: {
-                Text("Second header")
-                    .fontWeight(.bold)
+                Section{
+                    Text("11")
+                    Text("12")
+                } header: {
+                    Text("Second header")
+                        .sectionHeaderStyle()
+                }
             }
-
         }
+        .navigationTitle("Einstellungen")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+public extension Text {
+    func sectionHeaderStyle() -> some View {
+        self
+            .font(.system(.title3))
+            .fontWeight(.bold)
+            .foregroundColor(.primary)
+            .textCase(nil)
     }
 }
 
