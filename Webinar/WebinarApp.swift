d3782ff11e6e0bef971e8a68cdac1fe2b5fa5b14
Klaus M. Rodewig <kmr@appnoe.com>
2023-02-27 14:43:23 +0100

    Initial Commit

---
+++ Webinar/WebinarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebinarApp.swift
+//  Webinar
+//
+//  Created by Klaus Rodewig on 27.02.23.
+//
+
+import SwiftUI
+
+@main
+struct WebinarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
