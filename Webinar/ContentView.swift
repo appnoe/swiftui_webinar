de32bdc50f668e5621415c6e0c72423de10d3eda
Klaus M. Rodewig <kmr@appnoe.com>
2023-02-27 14:46:05 +0100

    add form
    Signed-off-by: Klaus M. Rodewig <kmr@appnoe.com>

--- Webinar/ContentView.swift
+++ Webinar/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form{
+            Text("1")
+            Text("2")
+            Text("3")
+            Text("4")
+            Text("5")
+            Text("6")
+            Text("7")
+            Text("8")
+            Text("9")
+            Text("10")
         }
-        .padding()
     }
 }
 
