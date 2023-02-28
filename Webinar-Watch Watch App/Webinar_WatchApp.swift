97445e9f85c49b65b2e596367460c7e51f4aa8e7
Klaus M. Rodewig <kmr@appnoe.com>
2023-02-28 10:07:17 +0100

    add watch target with basic List and NavigationLink
    Signed-off-by: Klaus M. Rodewig <kmr@appnoe.com>

---
+++ Webinar-Watch Watch App/Webinar_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Webinar_WatchApp.swift
+//  Webinar-Watch Watch App
+//
+//  Created by Klaus Rodewig on 28.02.23.
+//
+
+import SwiftUI
+
+@main
+struct Webinar_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
